be751f08ad20a33be6ba34d2a510ad14be10ea67
kozyrevayana99-ship-it <kozyrevayana99@gmail.com>
2026-09-02 05:40:43 -0700

    Update KlsKinescopePlayerFactory.kt

--- android/src/main/kotlin/ru/kls/kinescope_android_player/KlsKinescopePlayerFactory.kt
+++ android/src/main/kotlin/ru/kls/kinescope_android_player/KlsKinescopePlayerFactory.kt
@@ -1,19 +1,32 @@
 package ru.kls.kinescope_android_player
 
 import android.content.Context
+
+import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.StandardMessageCodec
 import io.flutter.plugin.platform.PlatformView
 import io.flutter.plugin.platform.PlatformViewFactory
 
-class KlsKinescopePlayerFactory : PlatformViewFactory(
+
+class KlsKinescopePlayerFactory(
+    private val messenger: BinaryMessenger
+) : PlatformViewFactory(
     StandardMessageCodec.INSTANCE
 ) {
+
     override fun create(
         context: Context,
         viewId: Int,
         args: Any?
     ): PlatformView {
-        val params = (args as? Map<*, *>) ?: emptyMap<String, Any?>()
-        return KlsKinescopePlayerView(context, params)
+
+        val params: Map<*, *> =
+            (args as? Map<*, *>)
+                ?: emptyMap<String, Any?>()
+
+        return KlsKinescopePlayerView(
+            context = context,
+            params = params
+        )
     }
 }
