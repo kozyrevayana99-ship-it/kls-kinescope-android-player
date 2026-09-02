79f18c65a24db03f1144ffd7d833fe963cb475bb
kozyrevayana99-ship-it <kozyrevayana99@gmail.com>
2026-09-02 05:42:51 -0700

    Update KlsKinescopeAndroidPlayerPlugin.kt

--- android/src/main/kotlin/ru/kls/kinescope_android_player/KlsKinescopeAndroidPlayerPlugin.kt
+++ android/src/main/kotlin/ru/kls/kinescope_android_player/KlsKinescopeAndroidPlayerPlugin.kt
@@ -2,19 +2,32 @@ package ru.kls.kinescope_android_player
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 
+
 class KlsKinescopeAndroidPlayerPlugin : FlutterPlugin {
+
     override fun onAttachedToEngine(
         flutterPluginBinding: FlutterPlugin.FlutterPluginBinding
     ) {
-        flutterPluginBinding.platformViewRegistry.registerViewFactory(
-            "kls_kinescope_android_player/native_player",
-            KlsKinescopePlayerFactory()
-        )
+
+        flutterPluginBinding
+            .platformViewRegistry
+            .registerViewFactory(
+                "kls_kinescope_android_player/native_player",
+                KlsKinescopePlayerFactory(
+                    flutterPluginBinding.binaryMessenger
+                )
+            )
     }
 
     override fun onDetachedFromEngine(
         binding: FlutterPlugin.FlutterPluginBinding
     ) {
-        // Each PlatformView releases its own player in dispose().
+        // Каждый PlatformView самостоятельно освобождает:
+        // - Kinescope player
+        // - listeners
+        // - lifecycle
+        // - PiP receiver
+        //
+        // Поэтому глобально здесь ничего release() не вызываем.
     }
 }
